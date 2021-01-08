91a9a2a72a6facab0d899830e5d37897f46d7b77
Дмитрий Тетерин <tet.dima.211@gmail.com>
2021-01-08 23:43:19 +0700

    Add start files.

---
+++ ClownCMS/ClientApp/src/components/Home.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var react_redux_1 = require("react-redux");
+var Home = function () { return (React.createElement("div", null,
+    React.createElement("h1", null, "Hello, world!"))); };
+exports.default = react_redux_1.connect()(Home);
+//# sourceMappingURL=Home.js.map
