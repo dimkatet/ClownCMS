83a5f3e775f6421f082b458ed39d17b7b927924b
StepanTarasenko <StepanTarasenko@outlook.com>
2021-01-15 17:24:58 +0700

    Create simple text editor

---
+++ ClownCMS/ClientApp/src/store/TestStore.ts
@@ -0,0 +1,41 @@
+﻿import { Action, Reducer } from 'redux';
+import { AppThunkAction } from './';
+
+export interface TestState {
+    text: string;
+}
+
+
+interface InitValueAction {
+    type: 'INIT_VALUE';
+    text: string;
+}
+
+
+type KnownAction = InitValueAction ;
+
+export const actionCreators = {
+    setValue: (text: string): AppThunkAction<KnownAction> => (dispatch, getState) => {
+        //const appState = getState();
+        dispatch({ type: 'INIT_VALUE', text: text });
+    }
+};
+
+const unloadedState: TestState = { text: ""};
+
+export const reducer: Reducer<TestState> = (state: TestState | undefined, incomingAction: Action): TestState => {
+    if (state === undefined) {
+        return unloadedState;
+    }
+
+    const action = incomingAction as KnownAction;
+    switch (action.type) {
+        case 'INIT_VALUE':
+            return {
+                text: action.text
+            };
+        default: return state;
+    }
+
+    //return state;
+};
--- ClownCMS/ClientApp/src/store/index.ts
+++ ClownCMS/ClientApp/src/store/index.ts
@@ -1,15 +1,18 @@
-﻿import * as StartPageStore from './StartPageStore';
+﻿import * as Projects from './Projects';
+import * as Test from './TestStore';
 
 // The top-level state object
 export interface ApplicationState {
-    startPage: StartPageStore.StartPageState | undefined;
+    projects: Projects.ProjectsState | undefined;
+    test: Test.TestState | undefined;
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    startPage: StartPageStore.reducer
+    projects: Projects.reducer,
+    test: Test.reducer
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
